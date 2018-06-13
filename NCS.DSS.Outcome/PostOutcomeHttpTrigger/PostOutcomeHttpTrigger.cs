6817a50d7d39643a1d943ed78722de9460ebe535
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-13 08:52:35 +0100

    Add Response Type to Post Method and updated Service to return new Guid

--- NCS.DSS.Outcome/PostOutcomeHttpTrigger/PostOutcomeHttpTrigger.cs
+++ NCS.DSS.Outcome/PostOutcomeHttpTrigger/PostOutcomeHttpTrigger.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -9,11 +11,23 @@ namespace NCS.DSS.Outcome.PostOutcomeHttpTrigger
     public static class PostOutcomeHttpTrigger
     {
         [FunctionName("Post")]
-        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Outcomes/")]HttpRequestMessage req, TraceWriter log)
+        [ResponseType(typeof(Models.Outcome))]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Customers/{customerId:guid}/Interactions/{interactionId:guid}/ActionPlans/{actionplanId:guid}/Outcomes/")]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("Post Outcome C# HTTP trigger function processed a request.");
 
-            return new HttpResponseMessage(HttpStatusCode.OK);
+            // Get request body
+            var outcome = await req.Content.ReadAsAsync<Models.Outcome>();
+
+            var outcomeService = new PostOutcomeHttpTriggerService();
+            var outcomeId = outcomeService.Create(outcome);
+
+            return outcomeId == null
+                ? new HttpResponseMessage(HttpStatusCode.BadRequest)
+                : new HttpResponseMessage(HttpStatusCode.Created)
+                {
+                    Content = new StringContent("Created Outcome record with Id of : " + outcomeId)
+                };
         }
     }
 }
--- NCS.DSS.Outcome/PostOutcomeHttpTrigger/PostOutcomeHttpTriggerService.cs
+++ NCS.DSS.Outcome/PostOutcomeHttpTrigger/PostOutcomeHttpTriggerService.cs
@@ -1,6 +1,15 @@
-﻿namespace NCS.DSS.Outcome.PostOutcomeHttpTrigger
+﻿using System;
+
+namespace NCS.DSS.Outcome.PostOutcomeHttpTrigger
 {
     public class PostOutcomeHttpTriggerService
     {
+        public Guid? Create(Models.Outcome outcome)
+        {
+            if (outcome == null)
+                return null;
+
+            return Guid.NewGuid();
+        }
     }
 }
