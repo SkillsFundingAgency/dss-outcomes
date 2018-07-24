4906b9a885d195eb2277b5741f59910927dc1555
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-07-24 16:32:28 +0100

    Add Unit Tests

---
+++ NCS.DSS.Outcomes.Tests/PatchOutcomesHttpTriggerTests.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NCS.DSS.Outcomes.Cosmos.Helper;
+using NCS.DSS.Outcomes.Helpers;
+using NCS.DSS.Outcomes.Models;
+using NCS.DSS.Outcomes.PatchOutcomesHttpTrigger.Service;
+using NCS.DSS.Outcomes.Validation;
+using Newtonsoft.Json;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+
+namespace NCS.DSS.Outcomes.Tests
+{
+    [TestFixture]
+    public class PatchOutcomesHttpTriggerTests
+    {
+        private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+        private const string ValidInteractionId = "1e1a555c-9633-4e12-ab28-09ed60d51cb3";
+        private const string ValidActionPlanId = "cff8080e-1da2-42bd-9b63-8f235aad9d86";
+        private const string ValidOutcomesId = "d5369b9a-6959-4bd3-92fc-1583e72b7e51";
+        private const string InValidId = "1111111-2222-3333-4444-555555555555";
+        private ILogger _log;
+        private HttpRequestMessage _request;
+        private IResourceHelper _resourceHelper;
+        private IValidate _validate;
+        private IHttpRequestMessageHelper _httpRequestMessageHelper;
+        private IPatchOutcomesHttpTriggerService _patchOutcomesHttpTriggerService;
+        private Models.Outcomes _outcome;
+        private OutcomesPatch _outcomePatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _outcome = Substitute.For<Models.Outcomes>();
+            _outcomePatch = Substitute.For<OutcomesPatch>();
+
+            _request = new HttpRequestMessage()
+            {
+                Content = new StringContent(string.Empty),
+                RequestUri = 
+                    new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
+                            $"Outcomes/1e1a555c-9633-4e12-ab28-09ed60d51cb3")
+            };
+
+            _log = Substitute.For<ILogger>();
+            _resourceHelper = Substitute.For<IResourceHelper>();
+            _validate = Substitute.For<IValidate>();
+            _httpRequestMessageHelper = Substitute.For<IHttpRequestMessageHelper>();
+            _patchOutcomesHttpTriggerService = Substitute.For<IPatchOutcomesHttpTriggerService>();
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(InValidId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenInteractionIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, InValidId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenAcionPlanIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, InValidId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenOutcomesIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, InValidId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenOutcomesHasFailedValidation()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            var validationResults = new List<ValidationResult> { new ValidationResult("interaction Id is Required") };
+            _validate.ValidateResource(Arg.Any<OutcomesPatch>()).Returns(validationResults);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual((HttpStatusCode)422, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenOutcomesRequestIsInvalid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Throws(new JsonException());
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual((HttpStatusCode)422, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeNoContent_WhenCustomerDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(false);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeNoContent_WhenInteractionDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeNoContent_WhenActionPlanDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(false);
+
+            _patchOutcomesHttpTriggerService.GetOutcomesForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeOk_WhenOutcomesDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _patchOutcomesHttpTriggerService.GetOutcomesForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenUnableToUpdateOutcomesRecord()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _patchOutcomesHttpTriggerService.GetOutcomesForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>(),Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult(_outcome).Result);
+
+            _patchOutcomesHttpTriggerService.UpdateAsync(Arg.Any<Models.Outcomes>(), Arg.Any<OutcomesPatch>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeOK_WhenRequestIsNotValid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _patchOutcomesHttpTriggerService.GetOutcomesForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult(_outcome).Result);
+
+            _patchOutcomesHttpTriggerService.UpdateAsync(Arg.Any<Models.Outcomes>(), Arg.Any<OutcomesPatch>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PatchOutcomesHttpTrigger_ReturnsStatusCodeOK_WhenRequestIsValid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<OutcomesPatch>(_request).Returns(Task.FromResult(_outcomePatch).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _patchOutcomesHttpTriggerService.GetOutcomesForCustomerAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<Guid>()).Returns(Task.FromResult(_outcome).Result);
+
+            _patchOutcomesHttpTriggerService.UpdateAsync(Arg.Any<Models.Outcomes>(), Arg.Any<OutcomesPatch>()).Returns(Task.FromResult(_outcome).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId, ValidOutcomesId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> RunFunction(string customerId, string interactionId, string actionPlanId, string outcomeId)
+        {
+            return await PatchOutcomesHttpTrigger.Function.PatchOutcomesHttpTrigger.Run(
+                _request, _log, customerId, interactionId, actionPlanId, outcomeId, _resourceHelper, _httpRequestMessageHelper, _validate, _patchOutcomesHttpTriggerService).ConfigureAwait(false);
+        }
+
+    }
+}
---
+++ NCS.DSS.Outcomes.Tests/PostOutcomesHttpTriggerTests.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NCS.DSS.Outcomes.Cosmos.Helper;
+using NCS.DSS.Outcomes.Helpers;
+using NCS.DSS.Outcomes.PostOutcomesHttpTrigger.Service;
+using NCS.DSS.Outcomes.Validation;
+using Newtonsoft.Json;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+
+namespace NCS.DSS.Outcomes.Tests
+{
+    [TestFixture]
+    public class PostOutcomesHttpTriggerTests
+    {
+        private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+        private const string ValidInteractionId = "1e1a555c-9633-4e12-ab28-09ed60d51cb3";
+        private const string ValidActionPlanId = "cff8080e-1da2-42bd-9b63-8f235aad9d86";
+        private const string InValidId = "1111111-2222-3333-4444-555555555555";
+        private ILogger _log;
+        private HttpRequestMessage _request;
+        private IResourceHelper _resourceHelper;
+        private IValidate _validate;
+        private IHttpRequestMessageHelper _httpRequestMessageHelper;
+        private IPostOutcomesHttpTriggerService _postOutcomesHttpTriggerService;
+        private Models.Outcomes _outcome;
+
+        [SetUp]
+        public void Setup()
+        {
+            _outcome = Substitute.For<Models.Outcomes>();
+
+            _request = new HttpRequestMessage()
+            {
+                Content = new StringContent(string.Empty),
+                RequestUri =
+                    new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
+                            $"Interactions/aa57e39e-4469-4c79-a9e9-9cb4ef410382/" +
+                            $"Outcomes")
+            };
+
+            _log = Substitute.For<ILogger>();
+            _resourceHelper = Substitute.For<IResourceHelper>();
+            _httpRequestMessageHelper = Substitute.For<IHttpRequestMessageHelper>();
+            _validate = Substitute.For<IValidate>();
+            _postOutcomesHttpTriggerService = Substitute.For<IPostOutcomesHttpTriggerService>();
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenCustomerIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(InValidId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenInteractionIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, InValidId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenActionPlanIdIsInvalid()
+        {
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, InValidId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+        
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenOutcomesHasFailedValidation()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            var validationResults = new List<ValidationResult> { new ValidationResult("interaction Id is Required") };
+            _validate.ValidateResource(Arg.Any<Models.Outcomes>()).Returns(validationResults);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual((HttpStatusCode)422, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeUnprocessableEntity_WhenOutcomesRequestIsInvalid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Throws(new JsonException());
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual((HttpStatusCode)422, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeNoContent_WhenCustomerDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(false);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeNoContent_WhenInteractionDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(false);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task GetOutcomesByIdHttpTrigger_ReturnsStatusCodeOk_WhenActionPlanDoesNotExist()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(false);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.NoContent, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeBadRequest_WhenUnableToCreateOutcomesRecord()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _postOutcomesHttpTriggerService.CreateAsync(Arg.Any<Models.Outcomes>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeCreated_WhenRequestIsInValid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _postOutcomesHttpTriggerService.CreateAsync(Arg.Any<Models.Outcomes>()).Returns(Task.FromResult<Models.Outcomes>(null).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Test]
+        public async Task PostOutcomesHttpTrigger_ReturnsStatusCodeCreated_WhenRequestIsValid()
+        {
+            _httpRequestMessageHelper.GetOutcomesFromRequest<Models.Outcomes>(_request).Returns(Task.FromResult(_outcome).Result);
+
+            _resourceHelper.DoesCustomerExist(Arg.Any<Guid>()).ReturnsForAnyArgs(true);
+            _resourceHelper.DoesInteractionExist(Arg.Any<Guid>()).Returns(true);
+            _resourceHelper.DoesActionPlanExist(Arg.Any<Guid>()).Returns(true);
+
+            _postOutcomesHttpTriggerService.CreateAsync(Arg.Any<Models.Outcomes>()).Returns(Task.FromResult<Models.Outcomes>(_outcome).Result);
+
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> RunFunction(string customerId, string interactionId, string actionPlanId)
+        {
+            return await PostOutcomesHttpTrigger.Function.PostOutcomesHttpTrigger.Run(
+                _request, _log, customerId, interactionId, actionPlanId, _resourceHelper, _httpRequestMessageHelper, _validate, _postOutcomesHttpTriggerService).ConfigureAwait(false);
+        }
+
+    }
+}
