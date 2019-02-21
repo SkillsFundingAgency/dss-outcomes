5f29ac07dd29f2c0bba7b6ce88de4bef2fcac097
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-02-21 20:43:39 +0000

    add missing patch service files

---
+++ NCS.DSS.Outcomes/PatchOutcomesHttpTrigger/Service/IOutcomePatchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCS.DSS.Outcomes.Models;
+
+namespace NCS.DSS.Outcomes.PatchOutcomesHttpTrigger.Service
+{
+    public interface IOutcomePatchService
+    {
+        string Patch(string outcomeJson, OutcomesPatch outcomePatch);
+    }
+}
---
+++ NCS.DSS.Outcomes/PatchOutcomesHttpTrigger/Service/OutcomePatchService.cs
@@ -0,0 +1,39 @@
+﻿using NCS.DSS.Outcomes.Helpers;
+using NCS.DSS.Outcomes.Models;
+using Newtonsoft.Json.Linq;
+
+namespace NCS.DSS.Outcomes.PatchOutcomesHttpTrigger.Service
+{
+    public class OutcomePatchService : IOutcomePatchService
+    {
+        public string Patch(string outcomeJson, OutcomesPatch outcomePatch)
+        {
+            if (string.IsNullOrEmpty(outcomeJson))
+                return null;
+
+            var obj = JObject.Parse(outcomeJson);
+
+            if (outcomePatch.OutcomeType.HasValue)
+                JsonHelper.UpdatePropertyValue(obj["OutcomeType"], outcomePatch.OutcomeType);
+
+            if (outcomePatch.OutcomeClaimedDate.HasValue)
+                JsonHelper.UpdatePropertyValue(obj["OutcomeClaimedDate"], outcomePatch.OutcomeClaimedDate.Value);
+
+            if (outcomePatch.OutcomeEffectiveDate.HasValue)
+                JsonHelper.UpdatePropertyValue(obj["OutcomeEffectiveDate"], outcomePatch.OutcomeEffectiveDate);
+
+           if (!string.IsNullOrEmpty(outcomePatch.TouchpointId))
+                JsonHelper.UpdatePropertyValue(obj["TouchpointId"], outcomePatch.TouchpointId);
+
+            if (outcomePatch.LastModifiedDate.HasValue)
+                JsonHelper.UpdatePropertyValue(obj["LastModifiedDate"], outcomePatch.LastModifiedDate);
+
+            if (!string.IsNullOrEmpty(outcomePatch.LastModifiedTouchpointId))
+                JsonHelper.UpdatePropertyValue(obj["LastModifiedTouchpointId"], outcomePatch.LastModifiedTouchpointId);
+
+            return obj.ToString();
+
+        }
+
+    }
+}
