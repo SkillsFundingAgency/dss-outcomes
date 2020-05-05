873e27e6083abd45a1f625b47213cd1289e4a7ad
anefiox <huss@anefiox.com>
2020-05-05 13:29:25 +0100

    Remove session validation

--- NCS.DSS.Outcomes/PatchOutcomesHttpTrigger/Function/PatchOutcomesHttpTrigger.cs
+++ NCS.DSS.Outcomes/PatchOutcomesHttpTrigger/Function/PatchOutcomesHttpTrigger.cs
@@ -265,16 +265,7 @@ namespace NCS.DSS.Outcomes.PatchOutcomesHttpTrigger.Function
             {
                 loggerHelper.LogInformationMessage(log, correlationGuid, "Action Plan Validation Object is null");
                 return httpResponseMessageHelper.UnprocessableEntity(req);
-            }
-
-            loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Attempting to get Session {0} for customer {1}", outcomeValidationObject.SessionId.GetValueOrDefault(), customerGuid));
-            var doesSessionExist = resourceHelper.DoesSessionExistAndBelongToCustomer(outcomeValidationObject.SessionId.GetValueOrDefault(), interactionGuid, customerGuid);
-
-            if (!doesSessionExist)
-            {
-                loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Session does not exist {0}", outcomeValidationObject.SessionId.GetValueOrDefault()));
-                return httpResponseMessageHelper.UnprocessableEntity(string.Format("Session ({0}) is not valid for interaction ({1}).", outcomeValidationObject.SessionId.GetValueOrDefault(), interactionGuid));
-            }
+         
 
             loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Attempting to get GetDateAndTimeOfSession for Session {0}", outcomeValidationObject.SessionId));
             var dateAndTimeOfSession = resourceHelper.GetDateAndTimeOfSession(outcomeValidationObject.SessionId.GetValueOrDefault());
--- NCS.DSS.Outcomes/PostOutcomesHttpTrigger/Function/PostOutcomesHttpTrigger.cs
+++ NCS.DSS.Outcomes/PostOutcomesHttpTrigger/Function/PostOutcomesHttpTrigger.cs
@@ -156,16 +156,6 @@ namespace NCS.DSS.Outcomes.PostOutcomesHttpTrigger.Function
                 return httpResponseMessageHelper.NoContent(interactionGuid);
             }
 
-            loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Attempting to get Session {0} for customer {1}", outcomesRequest.SessionId.GetValueOrDefault(), customerGuid));
-            var doesSessionExist = resourceHelper.DoesSessionExistAndBelongToCustomer(outcomesRequest.SessionId.GetValueOrDefault(), interactionGuid, customerGuid);
-
-            if (!doesSessionExist)
-            {
-                loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Session does not exist {0}", outcomesRequest.SessionId.GetValueOrDefault()));
-                //return httpResponseMessageHelper.UnprocessableEntity(string.Format("Session ({0}) is not valid for interaction ({1}).", outcomesRequest.SessionId.GetValueOrDefault(), interactionGuid));
-                return httpResponseMessageHelper.NoContent(outcomesRequest.SessionId.GetValueOrDefault());
-            }
-
             loggerHelper.LogInformationMessage(log, correlationGuid, string.Format("Attempting to get GetDateAndTimeOfSession for Session {0}", outcomesRequest.SessionId));
             var dateAndTimeOfSession = resourceHelper.GetDateAndTimeOfSession(outcomesRequest.SessionId.GetValueOrDefault());
 
