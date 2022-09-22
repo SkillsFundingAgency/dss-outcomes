a265618efda181fa67177f2a2b73406f20defba3
Daniel Puscas <danielpuscasjr99@gmail.com>
2022-09-22 15:00:44 +0100

    regular expression commente

--- NCS.DSS.Outcomes/Models/Outcomes.cs
+++ NCS.DSS.Outcomes/Models/Outcomes.cs
@@ -31,7 +31,7 @@ namespace NCS.DSS.Outcomes.Models
         
         [Required]
         [StringLength(50)]
-        [RegularExpression(@"(?<!\d)\d{8}(?!\d)")]
+        //[RegularExpression(@"(?<!\d)\d{8}(?!\d)")]
         [Display(Description = "Identifier supplied by the touchpoint to indicate their subcontractor")]
         [Example(Description = "01234567899876543210")]
         public string SubcontractorId { get; set; }
