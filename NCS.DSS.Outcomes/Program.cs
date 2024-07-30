f64ea3def644180e4770de0c747a44a312f25f11
hnandra <32364605+hnandra@users.noreply.github.com>
2024-07-30 11:27:41 +0100

    Added logging DI

--- NCS.DSS.Outcomes/Program.cs
+++ NCS.DSS.Outcomes/Program.cs
@@ -1,9 +1,12 @@
+using System.Linq;
 using DFC.Common.Standard.Logging;
 using DFC.HTTP.Standard;
 using DFC.JSON.Standard;
 using DFC.Swagger.Standard;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NCS.DSS.Outcomes.Cosmos.Helper;
 using NCS.DSS.Outcomes.Cosmos.Provider;
 using NCS.DSS.Outcomes.GetOutcomesByIdHttpTrigger.Service;
@@ -17,6 +20,8 @@ var host = new HostBuilder()
     .ConfigureServices(services =>
     {
         services.AddLogging();
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
         services.AddSingleton<IResourceHelper, ResourceHelper>();
         services.AddSingleton<IValidate, Validate>();
         services.AddSingleton<ILoggerHelper, LoggerHelper>();
@@ -29,8 +34,21 @@ var host = new HostBuilder()
         services.AddScoped<IGetOutcomesByIdHttpTriggerService, GetOutcomesByIdHttpTriggerService>();
         services.AddScoped<IPostOutcomesHttpTriggerService, PostOutcomesHttpTriggerService>();
         services.AddScoped<IPatchOutcomesHttpTriggerService, PatchOutcomesHttpTriggerService>();
-        services.AddScoped<IPatchOutcomesHttpTriggerService, PatchOutcomesHttpTriggerService>();
         services.AddScoped<IOutcomePatchService, OutcomePatchService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+        // For more information, see https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=windows#application-insights
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
