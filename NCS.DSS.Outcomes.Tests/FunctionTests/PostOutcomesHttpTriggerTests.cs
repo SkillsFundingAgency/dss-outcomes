d457eb069a60312f0569e7d7e3c3ae05b65d0010
hnandra <h_nandra@hotmail.co.uk>
2026-02-16 16:50:59 +0000

    Revert "Merge pull request #102 from SkillsFundingAgency/DSS-Release-106-v4"
    This reverts commit 3627a3ea6017ab0adb083f329da28af8a5ff6839, reversing
    changes made to 5f02ff0b01c43c132f9fe0df2bdfc605e4f1955d.

--- NCS.DSS.Outcomes.Tests/FunctionTests/PostOutcomesHttpTriggerTests.cs
+++ NCS.DSS.Outcomes.Tests/FunctionTests/PostOutcomesHttpTriggerTests.cs
@@ -186,9 +186,9 @@ namespace NCS.DSS.Outcomes.Tests.FunctionTests
             var result = await RunFunction(ValidCustomerId, ValidInteractionId, ValidActionPlanId);
 
             // Assert
-            Assert.That(result, Is.InstanceOf<NotFoundObjectResult>());
+            Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
         }
-    
+
         [Test]
         public async Task GetOutcomesByIdHttpTrigger_ReturnsStatusCodeNotFound_WhenActionPlanDoesNotExist()
         {
